4fc8ab2c9d6d9449b1c692313b5387a8cc289c4c
Aman0246jploft <aman_kashyap@jploft.in>
2025-06-12 12:14:54 +0530

    fixed

--- routes/services/serviceGlobalCURD.js
+++ routes/services/serviceGlobalCURD.js
@@ -202,7 +202,7 @@ const getAllDocuments = async (
         }
 
 
-        console.log('finalQuery1',finalQuery,options)
+        console.log('finalQuery1', finalQuery, options)
 
 
         // Query builder
